1d7019912f81f31743f04ad043f66816db1d959c
Cédric Noël <cedric.noel.paris@gmail.com>
2019-02-14 15:22:24 +0100

    Add groups

--- src/Entity/Model.php
+++ src/Entity/Model.php
@@ -6,10 +6,25 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get"={"access_control"="is_granted('ROLE_USER')"},
+ *         "post"={"validation_groups"={"Default", "postValidation"},
+ *                  {"access_control"="is_granted('ROLE_USER')"}
+ *          }
+ *     },
+ *     itemOperations={
+ *         "delete",
+ *         "get"={"access_control"="is_granted('ROLE_USER') "},
+ *         "put"={"validation_groups"={"Default", "putValidation"}}
+ *     },
+ *     normalizationContext={"groups"={"read_model"}},
+ *     denormalizationContext={"groups"={"write_model"}}
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\ModelRepository")
  */
 class Model
@@ -20,7 +35,6 @@ class Model
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=30)
      * @Assert\NotBlank
@@ -28,9 +42,9 @@ class Model
      *      max = 50,
      *      maxMessage = "max {{ limit }} characters"
      * )
+     * @Groups({"read_model", "write_model"})
      */
     private $name;
-
     /**
      * @ORM\Column(type="integer")
      * @Assert\NotBlank
@@ -38,30 +52,51 @@ class Model
      *     type="integer",
      *     message="{{ value }} veuillez saisir un entier."
      * )
+     * @Groups({"read_model", "write_model"})
      */
     private $sits;
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Plane", mappedBy="model")
      * @Assert\NotBlank
+     * @Groups({"read_model"})
      */
     private $planes;
 
+    /**
+     * Model constructor
+     */
     public function __construct()
     {
         $this->planes = new ArrayCollection();
     }
 
+    /**
+     * getId function
+     *
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * getName function
+     *
+     * @return null|string
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * setName function
+     *
+     * @param string $name
+     *
+     * @return Model
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -69,11 +104,23 @@ class Model
         return $this;
     }
 
+    /**
+     * getSits function
+     *
+     * @return int|null
+     */
     public function getSits(): ?int
     {
         return $this->sits;
     }
 
+    /**
+     * setSits function
+     *
+     * @param int $sits
+     *
+     * @return Model
+     */
     public function setSits(int $sits): self
     {
         $this->sits = $sits;
@@ -89,26 +136,26 @@ class Model
         return $this->planes;
     }
 
-//    public function addPlane(Plane $plane): self
-//    {
-//        if (!$this->planes->contains($plane)) {
-//            $this->planes[] = $plane;
-//            $plane->setModel($this);
-//        }
-//
-//        return $this;
-//    }
-//
-//    public function removePlane(Plane $plane): self
-//    {
-//        if ($this->planes->contains($plane)) {
-//            $this->planes->removeElement($plane);
-//            // set the owning side to null (unless already changed)
-//            if ($plane->getModel() === $this) {
-//                $plane->setModel(null);
-//            }
-//        }
-//
-//        return $this;
-//    }
+    //    public function addPlane(Plane $plane): self
+    //    {
+    //        if (!$this->planes->contains($plane)) {
+    //            $this->planes[] = $plane;
+    //            $plane->setModel($this);
+    //        }
+    //
+    //        return $this;
+    //    }
+    //
+    //    public function removePlane(Plane $plane): self
+    //    {
+    //        if ($this->planes->contains($plane)) {
+    //            $this->planes->removeElement($plane);
+    //            // set the owning side to null (unless already changed)
+    //            if ($plane->getModel() === $this) {
+    //                $plane->setModel(null);
+    //            }
+    //        }
+    //
+    //        return $this;
+    //    }
 }
--- src/Entity/Staff.php
+++ src/Entity/Staff.php
@@ -7,9 +7,24 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get"={"access_control"="is_granted('ROLE_ADMIN')"},
+ *         "post"={"validation_groups"={"Default", "postValidation"},
+ *                  {"access_control"="is_granted('ROLE_ADMIN')"}
+ *          }
+ *     },
+ *     itemOperations={
+ *         "delete",
+ *         "get"={"access_control"="is_granted('ROLE_ADMIN') "},
+ *         "put"={"validation_groups"={"Default", "putValidation"}}
+ *     },
+ *     normalizationContext={"groups"={"read_staff"}},
+ *     denormalizationContext={"groups"={"write_staff"}}
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\StaffRepository")
  */
 class Staff
@@ -20,7 +35,6 @@ class Staff
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank
@@ -29,9 +43,9 @@ class Staff
      *      minMessage = "min {{ limit }} characters ",
      *      maxMessage = "max {{ limit }} characters"
      * )
+     * @Groups("read_staff", "write_staff")
      */
     private $firstname;
-
     /**
      * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank
@@ -41,11 +55,9 @@ class Staff
      *      minMessage = "Your first name must be at least {{ limit }} characters long",
      *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
      * )
+     * @Groups("read_staff", "write_staff")
      */
     private $lastname;
-
-
-
     /**
      * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank
@@ -55,30 +67,50 @@ class Staff
      *      minMessage = "Your first name must be at least {{ limit }} characters long",
      *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
      * )
+     * @Groups("read_staff", "write_staff")
      */
     private $job;
-
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Crew", mappedBy="staffs")
-     * @Assert\NotBlank
+     * @Groups("read_staff")
      */
     private $crews;
 
+    /**
+     * Staff constructor
+     */
     public function __construct()
     {
         $this->crews = new ArrayCollection();
     }
 
+    /**
+     * getId function
+     *
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * getFirstname function
+     *
+     * @return null|string
+     */
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
 
+    /**
+     * setFirstname function
+     *
+     * @param string $firstname
+     *
+     * @return Staff
+     */
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
@@ -86,11 +118,23 @@ class Staff
         return $this;
     }
 
+    /**
+     * getLastname function
+     *
+     * @return null|string
+     */
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
 
+    /**
+     * setLastname function
+     *
+     * @param string $lastname
+     *
+     * @return Staff
+     */
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
@@ -98,12 +142,23 @@ class Staff
         return $this;
     }
 
-
+    /**
+     * getJob function
+     *
+     * @return null|string
+     */
     public function getJob(): ?string
     {
         return $this->job;
     }
 
+    /**
+     * setJob function
+     *
+     * @param string $job
+     *
+     * @return Staff
+     */
     public function setJob(string $job): self
     {
         $this->job = $job;
@@ -119,23 +174,23 @@ class Staff
         return $this->crews;
     }
 
-//    public function addCrew(Crew $crew): self
-//    {
-//        if (!$this->crews->contains($crew)) {
-//            $this->crews[] = $crew;
-//            $crew->addStaff($this);
-//        }
-//
-//        return $this;
-//    }
-//
-//    public function removeCrew(Crew $crew): self
-//    {
-//        if ($this->crews->contains($crew)) {
-//            $this->crews->removeElement($crew);
-//            $crew->removeStaff($this);
-//        }
-//
-//        return $this;
-//    }
+    //    public function addCrew(Crew $crew): self
+    //    {
+    //        if (!$this->crews->contains($crew)) {
+    //            $this->crews[] = $crew;
+    //            $crew->addStaff($this);
+    //        }
+    //
+    //        return $this;
+    //    }
+    //
+    //    public function removeCrew(Crew $crew): self
+    //    {
+    //        if ($this->crews->contains($crew)) {
+    //            $this->crews->removeElement($crew);
+    //            $crew->removeStaff($this);
+    //        }
+    //
+    //        return $this;
+    //    }
 }
