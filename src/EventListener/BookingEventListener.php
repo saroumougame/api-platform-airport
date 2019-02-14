dbfea22f04cf1d50f23bb1bbd9f7628c88c690cc
Cédric Noël <cedric.noel.paris@gmail.com>
2019-02-14 12:14:10 +0100

    Correct ticket generation

--- src/EventListener/BookingEventListener.php
+++ src/EventListener/BookingEventListener.php
@@ -4,44 +4,61 @@ namespace App\EventListener;
 
 use App\Entity\Booking;
 use App\Entity\Ticket;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 
 class BookingEventListener
 {
+    /** @var EntityManagerInterface $em */
+    private $em;
+
+    /**
+     * BookingEventListener constructor
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * onKernelView function
      *
      * @param GetResponseForControllerResultEvent $event
-     * @param ObjectManager $manager
      *
      * @return void
+     * @internal param ObjectManager $manager
      */
-    public function onKernelView(GetResponseForControllerResultEvent $event, ObjectManager $manager)
+    public function onKernelView(GetResponseForControllerResultEvent $event)
     {
         /** @var Booking $user */
         $booking = $event->getControllerResult();
-        /** @var string $bookingStatus */
-        $bookingStatus = $booking->getStatus();
-        if ($booking instanceof Booking && $bookingStatus == '') {
-            $booking->setStatus('processing');
+
+        if ($booking instanceof Booking) {
+            /** @var string $bookingStatus */
+            $bookingStatus = $booking->getStatus();
+
+            if ($bookingStatus == '') {
+                $booking->setStatus('processing');
+            }
+
+            /** @var Ticket $ticket */
+            $ticket = new Ticket();
+            $ticket->setReference($this->random());
+            $ticket->setCustomer($booking->getCustomer());
+            $ticket->setFlight($booking->getFlight());
+
+            /** @var int $sitRange */
+            $sitRange = $booking->getFlight()->getPlane()->getModel()->getSits();
+            /** @var int $sit */
+            $sit = rand(1, $sitRange);
+
+            $ticket->setSit($sit);
+
+            $this->em->persist($ticket);
+            $this->em->flush();
         }
-
-        /** @var Ticket $ticket */
-        $ticket = new Ticket();
-        $ticket->setReference($this->random());
-        $ticket->setCustomer($booking->getCustomer());
-        $ticket->setFlight($booking->getFlight());
-
-        /** @var int $sitRange */
-        $sitRange = $booking->getFlight()->getPlane()->getModel()->getSits();
-        /** @var int $sit */
-        $sit = rand(1, $sitRange);
-
-        $ticket->setSit($sit);
-
-        $manager->persist($ticket);
-        $manager->flush();
     }
 
     /**
